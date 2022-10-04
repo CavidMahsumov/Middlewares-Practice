d902d840fb7309472202731bcbbc31575a5bc714
Cavid <mehsumovcavid@gmail.com>
2022-10-04 22:02:25 +0400

    Add project files.

---
+++ MiddleWare/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace MiddleWare.Middlewares
+{
+    public class RequestEditingMiddleware
+    {
+        private  RequestDelegate _requestDelegate;
+
+        public RequestEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.ToString() == "/Cavid")
+                await context.Response.WriteAsync("Salam Radnoyy");
+            else
+                await _requestDelegate.Invoke(context);
+        }
+    }
+}
---
+++ MiddleWare/Middlewares/ResponseEditingMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MiddleWare.Middlewares
+{
+    public class ResponseEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+
+        public ResponseEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _requestDelegate(context);
+            if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+                 await context.Response.WriteAsync("Page Not Found");
+        }
+    }
+}
